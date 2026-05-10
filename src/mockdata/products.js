3017f46f9759cb0ef123e25a281377658ed27112
Manuela Ortega <mamuou24@gmail.com>
2026-05-10 02:29:24 -0500

    feat: agregar productos y fotos reales de Antojos Tropicales

---
+++ src/mockdata/products.js
@@ -0,0 +1,36 @@
+// src/mockdata/products.js
+
+export const mockProducts = [
+  {
+    id: 1,
+    name: "Mix Verde",
+    price: 12000,
+    description: "Refrescante combinación de guayaba verde, mango y manzana verde.",
+    category: "Frutas",
+    image: "/images/guayaba verde-mango-manzana verde.jpeg"
+  },
+  {
+    id: 2,
+    name: "Explosión Tropical",
+    price: 13000,
+    description: "Dulce de mango mezclado con la acidez del maracuyá y piña.",
+    category: "Frutas",
+    image: "/images/mango dulce-maracuya-piña.jpeg"
+  },
+  {
+    id: 3,
+    name: "Trío de Frutas Ácidas",
+    price: 11500,
+    description: "Mango, piña y manzana verde.",
+    category: "Frutas",
+    image: "/images/mango-piña-manzana verde.jpeg"
+  },
+  {
+    id: 4,
+    name: "Mango, Uva y Naranja",
+    price: 12500,
+    description: "Mango fresco acompañado de uvas y naranja.",
+    category: "Frutas",
+    image: "/images/mango-uva-naranja.jpeg"
+  }
+];
