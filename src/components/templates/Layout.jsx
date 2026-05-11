2629f52d7b8868d585478395b3791797991717f7
jorcashe <89223516+jorcashe@users.noreply.github.com>
2026-05-11 16:23:31 -0500

    preparar el Navbar como global

--- src/components/templates/Layout.jsx
+++ src/components/templates/Layout.jsx
@@ -1,13 +1,27 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../organisms/NavBar/NavBar';
 
-export default function Layout() {
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../organisms/Navbar' // La ubicación del Navbar que hicimos
+
+const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <NavBar />
-      <main className="flex-1 w-full">
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar global que persiste en todas las rutas */}
+      <Navbar />
+      
+      {/* Contenido dinámico de las páginas */}
+      <main className="flex-grow container mx-auto px-4 py-6">
         <Outlet />
       </main>
+
+      {/* Aquí podrías agregar un Footer más tarde */}
+      <footer className="bg-gray-800 text-white p-4 text-center">
+        <p>&copy; 2026 Antojos Tropicales - Proyecto E-commerce</p>
+      </footer>
     </div>
-  );
+  )
 }
+
+export default Layout
